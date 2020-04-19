586df1ca2131fb51503b9949d67e81e9f1784ab8
Titux Metal <github@lgdweb.fr>
2020-04-19 03:22:26 +0200

    [REFACTOR] Update index.js file; make api working

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-const next = require('next')
-
-const { port, isProd } = require('./config')
-const dev = !isProd
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const { port } = require('./config')
 
 require('./database')()
 
 const server = require('./app')
 
-app.prepare().then(() => {
-  server.get('/_next/*', (req, res) => handle(req, res))
-  server.get('/static/*', (req, res) => handle(req, res))
-  server.get('*', (req, res) => handle(req, res))
-
-  server.listen(port, err => {
-    if (err) {
-      throw err
-    }
-    console.log(`Running on port ${port}`)
-  })
-})
+server.listen(port, () => console.log(`Server is running on port: ${port}`))
