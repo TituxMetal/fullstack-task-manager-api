cc686b7b271a219b97ddbe6e8ace28bf0e8c98c9
Titux Metal <github@lgdweb.fr>
2020-06-07 10:45:46 +0200

    Update app.js file; call directly the routes functions and pass it app

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 import express from 'express'
 
-import tasksRoute from '~/routes/task'
-import welcomeRoute from '~/routes/welcome'
+import { taskRoutes, welcomeRoutes } from '~/routes'
 
 export const createApp = () => {
   const app = express()
 
   app.use(express.json())
 
-  app.use('/api', welcomeRoute)
-  app.use('/api/tasks', tasksRoute)
+  taskRoutes(app)
+  welcomeRoutes(app)
 
   return app
 }
