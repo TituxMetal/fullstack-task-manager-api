1c28b3a1c68f7b9d052aacb46cb1691fd1ed22a6
Titux Metal <github@lgdweb.fr>
2020-06-09 05:47:43 +0200

    Implementation of error management:
    * Updating app.js file; use of NotFoundError class, errorHandler middleware and express-async-errors package
    * Adding index.js file in middlewares; exports errorHandler middleware
    * Adding errorHandler.js file in middlewares; an error handler middleware
    * Updating package.json file; adding common-express-helpers and express-async-errors dependencies

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
+import { NotFoundError } from '@lgdweb/common-express-helpers'
 import express from 'express'
+import 'express-async-errors'
 
+import { errorHandler } from './middlewares'
 import { taskRoutes, welcomeRoutes } from '~/routes'
 
 export const createApp = () => {
@@ -9,6 +12,12 @@ export const createApp = () => {
 
   taskRoutes(app)
   welcomeRoutes(app)
+
+  app.all('*', async (req, _res) => {
+    throw new NotFoundError(req.path)
+  })
+
+  app.use(errorHandler)
 
   return app
 }
