f36b64b09281c8ed3ddcab9378540d1c0d621747
Titux Metal <github@lgdweb.fr>
2021-05-23 17:06:28 +0200

    Updating index.js file; changes the way the server is running

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-import { createApp } from './app'
 import { port } from '~/config'
 import connectDb from '~/database'
-import { localISOTime, shutdownServer } from './utils'
+import { setCloseOnExit } from '~/utils'
 
-const app = createApp()
+import { createApp } from './app'
 
-app.listen(port, '0.0.0.0', () => {
-  connectDb()
+const server = createApp()
 
-  console.info(`Server is listening on http://localhost:${port}`)
-
-  process.on('SIGINT', () => {
-    console.info('Got SIGINT. Graceful shutdown.', localISOTime())
-    shutdownServer(app)
-  })
-
-  process.on('SIGTERM', () => {
-    console.info('Got SIGTERM. Graceful shutdown.', localISOTime())
-    shutdownServer(app)
-  })
-})
+connectDb()
+setCloseOnExit(server)
