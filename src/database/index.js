21731e12fe167d5f4d331aadde058da581401dd7
Titux Metal <github@lgdweb.fr>
2021-05-23 18:37:48 +0200

    Updating the mongo db connection function

--- src/database/index.js
+++ src/database/index.js
@@ -1,18 +1,1 @@
-import mongoose from 'mongoose'
-
-import { inProd, mongoOptions, mongoUri } from '~/config'
-
-const connectDb = async () => {
-  try {
-    await mongoose.connect(mongoUri, mongoOptions)
-
-    console.info(`
-      Database connected!
-      ${!inProd && mongoUri}
-    `)
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-export default connectDb
+export { default as connectMongoDb } from './connectMongoDb'
