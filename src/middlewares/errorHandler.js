970b506e3621c99455be4a0caf1dc755b97ae9a4
Titux Metal <github@lgdweb.fr>
2021-05-23 16:03:45 +0200

    Features/error handler (#11)
    * Updating dependencies; adds http-errors and removes common-express-helpers
    
    * Adding getErrorDetails.js file in utils; a utility for getting error details nicely formatted by http-errors
    
    * Updating errorHandler.js file in middleware; handles errors, formats the response with error details, sets the header content type to application/problem+json and adds informations in the console for mongo errors
    
    * Updating requestValidator.js file in middlewares; uses http-errors
    
    * Updating app.js file; uses http-errors

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,11 +1,38 @@
-import { CustomError } from '@lgdweb/common-express-helpers'
+import { getErrorDetails } from '~/utils'
 
-const errorHandler = (err, _req, res, _next) => {
-  if (err instanceof CustomError) {
-    return res.status(err.statusCode).json({ errors: err.serializeErrors() })
+const errorHandler = (error, req, res, next) => {
+  if (req.headersSent) {
+    return next(error)
   }
 
-  return res.status(500).json({ errors: [{ message: `Something went wrong: ${err.message}` }] })
+  const errorDetails = getErrorDetails(error)
+
+  if (error.code === 11000) {
+    console.error('An error occured from mongoDb:', {
+      type: 'Duplicate Field Error',
+      reason: 'One or more fields have value that already exists.',
+      fields: Object.keys(error.keyValue)
+    })
+  }
+
+  if (error.kind === 'ObjectId') {
+    console.error('An error occured from mongoDb:', {
+      type: 'Cast to ObjectId failed',
+      reason:
+        'Argument passed in must be a single String of 12 bytes or a string of 24 hex characters',
+      fields: error.path
+    })
+  }
+
+  if (!errorDetails.status) {
+    errorDetails.status = error.statusCode || 500
+  }
+
+  res.set('Content-Type', 'application/problem+json')
+
+  res.status(errorDetails.status).json(errorDetails)
+
+  return next()
 }
 
 export default errorHandler
