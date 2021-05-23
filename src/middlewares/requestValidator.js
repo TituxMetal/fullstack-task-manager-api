5565089583b691e33d91643b4462630f8545a2fc
Titux Metal <github@lgdweb.fr>
2021-05-23 15:57:34 +0200

    Updating requestValidator.js file in middlewares; uses http-errors

--- src/middlewares/requestValidator.js
+++ src/middlewares/requestValidator.js
@@ -1,16 +1,18 @@
-import { RequestValidationError } from '@lgdweb/common-express-helpers'
 import { validationResult } from 'express-validator'
+import httpError from 'http-errors'
 
-const requestValidator = validationRules => async (req, _res, next) => {
+const requestValidator = validationRules => async (req, res, next) => {
   await Promise.all(validationRules.map(validation => validation.run(req)))
 
   const errors = validationResult(req)
 
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array())
+  if (errors.isEmpty()) {
+    return next()
   }
 
-  next()
+  const errorType = errors.mapped().id ? 400 : 422
+
+  return next(httpError(errorType, { reason: errors.array() }))
 }
 
 export default requestValidator
